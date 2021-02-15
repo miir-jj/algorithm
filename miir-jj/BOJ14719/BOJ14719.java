0b551a3b9033ec23286849a5e7ce10a927a45855
miir-jj <mrjin111@gmail.com>
2021-02-15 18:38:28 +0900

    진미령 / 2주차

--- miir-jj/BOJ14719/BOJ14719.java
+++ miir-jj/BOJ14719/BOJ14719.java
@@ -20,16 +20,17 @@ public class BOJ14719 {
 			height[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		int tmp=0, result=0;
+		int h1, h2, tmp=0, result=0;
 		for(int i=1;i<W-1;i++) {
-			int h1=0,h2=0;
+			h1=0;
+			h2=0;
 			for(int j=0;j<i;j++) {
 				h1=Math.max(h1, height[j]); //현재 위치 제외 양옆의 높이 최댓값을 구함
 			}
 			for(int j=i+1;j<W;j++) {
 				h2=Math.max(h2, height[j]);
 			}
-			tmp=(Math.min(h1,h2)-height[i]); //양 옆 높이 중 더 낮은 높이에서 현재 높이 빼줌
+			tmp=(Math.min(h1,h2)-height[i]); //양 옆 높이 중 최솟값에서 현재 높이 빼줌
 			if(tmp>0)	////빗물이 저장 될 경우
 				result+=tmp;
 		}
